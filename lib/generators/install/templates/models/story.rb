45c1e68933171be32434e2fe2620b2aecb575297
Phelipe <phwener@gmail.com>
2019-03-27 01:37:45 -0300

    Add node options to story

--- lib/generators/install/templates/models/story.rb
+++ lib/generators/install/templates/models/story.rb
@@ -8,4 +8,14 @@ class Story < ApplicationRecord
   def self.plugin_name
     'Kanban'
   end
+
+  # See Artifact#node_options
+  def node_options
+    {
+      color: {
+        border: '##34495e',
+        background: last_version ? '#16a085' : '#7f8c8d'
+      }
+    }
+  end
 end
