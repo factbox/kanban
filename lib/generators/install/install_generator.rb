7870d4e21e708ed685ef7541a908e66eda9d8218
Phelipe <phwener@gmail.com>
2019-04-03 03:17:55 -0300

    Update generators

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -2,15 +2,31 @@ require 'rails/generators'
 require 'rails/generators/base'
 require 'rails/generators/active_record'
 
+# Generator to install all code that plugin needs to work
 class InstallGenerator < Rails::Generators::NamedBase
   include ActiveRecord::Generators::Migration
 
-  source_root File.expand_path("../templates", __FILE__)
+  source_root File.expand_path('../templates', __FILE__)
 
   def create_migrations
     get_dir_path('migrations').sort.each do |filepath|
       name = File.basename(filepath)
       migration_template "migrations/#{name}", "db/migrate/#{name}", skip: true
+    end
+  end
+
+  def create_controllers
+    get_dir_path('controllers').sort.each do |filepath|
+      name = File.basename(filepath)
+      copy_file "controllers/#{name}", "app/controllers/#{name}"
+    end
+  end
+
+  def create_components
+    get_dir_path('javascript').sort.each do |filepath|
+      name = File.basename(filepath)
+      copy_file "javascript/components/#{name}",
+                "app/javascript/components/#{name}"
     end
   end
 
@@ -22,17 +38,17 @@ class InstallGenerator < Rails::Generators::NamedBase
   end
 
   def create_views
-    directory "views/stories", "app/views/stories"
+    directory 'views/stories', 'app/views/stories'
   end
 
   def create_routes
     route "resources :stories, controller: 'artifacts'"
+    route "get '/kanban/:id/move/:layer', to: 'layers#move'"
   end
 
   private
 
-    def get_dir_path(folder)
-      Dir["#{self.class.source_root}/#{folder}/*.rb"]
-    end
-
+  def get_dir_path(folder)
+    Dir["#{self.class.source_root}/#{folder}/*.rb"]
+  end
 end
--- lib/generators/install/templates/controllers/layers_controller.rb
+++ lib/generators/install/templates/controllers/layers_controller.rb
@@ -2,7 +2,9 @@
 class LayersController < ActionController::API
   before_action :set_story, only: [:move_story]
 
-  def move_story
+  # GET /kanban/:id/move/:layer
+  # Move specific artifact to layer
+  def move
     if @story.update(layer: params[:destiny])
       render status: 200, json: { message: 'Story moved!' }.to_json
     else
